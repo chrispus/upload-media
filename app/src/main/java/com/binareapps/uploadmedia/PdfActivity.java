3cd502d8fc17271df5c931474213bf264c893155
chrispus <chrispuspeter@yahoo.com>
2020-04-28 15:29:44 +0300

    Initial Commit

---
+++ app/src/main/java/com/binareapps/uploadmedia/PdfActivity.java
@@ -0,0 +1,13 @@
+package com.binareapps.uploadmedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PdfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pdf_layout);
+    }
+}
